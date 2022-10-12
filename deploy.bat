e04aa29314406aa022c709a41722f5194a550b35
GordienkoAnastasia <gordienko_anastasia99@mail.ru>
2022-10-13 00:14:01 +0700

    deploy_workflow

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+cd ./dist
+git init
+git checkout -B master
+git add -A
+git commit -m "latest site deloy"
+git push -f https://github.com/GordienkoAnastasia/open-layer-app.git master:www_dist
+cd ..
