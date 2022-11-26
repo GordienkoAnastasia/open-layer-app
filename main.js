e552e0918f6c2097398cec3648d5669f3e8adc24
GordienkoAnastasia <gordienko_anastasia99@mail.ru>
2022-11-26 18:33:43 +0700

    вставка красной линии

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import { fromLonLat } from 'ol/proj';
+import TileWMS from 'ol/source/TileWMS';
 import OSM from 'ol/source/OSM';
 
 const map = new Map({
@@ -9,7 +10,16 @@ const map = new Map({
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    new TileLayer({
+        source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/study/wms',
+        params: {'LAYERS': 'study:red_border', 'TILED': true}, // слой с красной линией Минусинска
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,
+      }),
+    }),
   ],
   view: new View({
     center: fromLonLat([91.6875, 53.7103]),
